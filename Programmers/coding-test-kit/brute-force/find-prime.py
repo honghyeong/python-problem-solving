d08b78dabf387301531134de28106307f65e7176
honghyeong <scarletgold0601@gmail.com>
2021-11-19 16:12:27 +0900

    완전탐색: 소수 찾기

---
+++ Programmers/coding-test-kit/brute-force/find-prime.py
@@ -0,0 +1,26 @@
+import itertools
+
+
+def solution(numbers):
+    answer = 0
+    permute = set()
+    # 가능한 문자열 순열을 permute에 추가
+    for i in range(1, len(numbers) + 1):
+        permute.update(list(map(''.join, itertools.permutations(list(numbers), i))))
+        # int로 변경했을때 겹치는 값 제거
+    permute = set(map(int, permute))
+
+    # 소수 체크 함수
+    def is_prime(number):
+        if number == 1 or number == 0:
+            return False
+        for i in range(2, number):
+            if (number % i == 0):
+                return False
+        return True
+
+    for per in permute:
+        if is_prime(per):
+            answer += 1
+
+    return answer
--- Programmers/coding-test-kit/brute-force/spj-test.py
+++ Programmers/coding-test-kit/brute-force/spj-test.py
@@ -1,21 +1,5 @@
-def solution(answers):
-    # 수포자 찍기 패턴
-    spj_1 = [1, 2, 3, 4, 5]
-    spj_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    spj_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    score = [0] * 3  # 각 수포자의 정답수
+import itertools
 
-    for answer in range(len(answers)):
-        if spj_1[answer % 5] == answers[answer]:
-            score[0] += 1
-        if spj_2[answer % 8] == answers[answer]:
-            score[1] += 1
-        if spj_3[answer % 10] == answers[answer]:
-            score[2] += 1
-    answer = []
-    for index, value in enumerate(score):
-        if (value == max(score)):  # 가장 높은 점수를 받은 경우 answer에 수포자 index 추가
-            answer.append(index + 1)
-    # 점수 sort
-    answer.sort()
-    return answer
+numbers="17"
+permute=set(map(''.join,itertools.permutations(list(numbers),1)))
+print(permute)
